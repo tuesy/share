aa4024f4b944d7f48fe01921b540907ec83eebb8
Jimmy Zhang <jimmy@gamefuse.com>
2014-09-23 23:25:35 -0700

    finish rake task to collect all app urls

--- lib/sharer.rb
+++ lib/sharer.rb
@@ -1,12 +1,21 @@
 class Sharer
   class << self
-    # collects all the app urls and outputs them to data/app_urls
+    # collects all the app urls and outputs them to data/app_urls.csv
     def app_urls
-      @@driver = Selenium::WebDriver.for :chrome
-      @@driver.navigate.to url
-      doc = Nokogiri::HTML(@@driver.page_source)
-      debugger
-      p 'done'
+      begin
+        url = 'https://share.oculusvr.com/category/all'
+        CSV.open('data/app_urls.csv', 'w') do |csv|
+          @@driver = Selenium::WebDriver.for :chrome
+          @@driver.navigate.to url
+          doc = Nokogiri::HTML(@@driver.page_source)
+          links = doc.css('.appPod .nameWrap a')
+          links.each do |x|
+            csv << ["https://share.oculusvr.com#{x[:href]}"]
+          end
+        end
+      ensure
+        @@driver.quit if @@driver
+      end
     end
   end
 end
