faa0663c56d48dcd2179c4dd5e5ff9ccaff7ccfe
Jimmy Zhang <jimmy@gamefuse.com>
2014-09-25 16:55:41 -0700

    hard-code featured list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,14 @@ class HomeController < ApplicationController
   def index
     @categories = [['All', 'title'], ['Newest', 'created_at'], ['Top Rated', 'rating']] + Category.all
     @genres = Genre.all
-    @featured = App.all[10..15]
+    @featured = [
+      App.find_by_title('Vanguard V'),
+      App.find_by_title('SightLine: The Chair'),
+      App.find_by_title('Blocked In'),
+      App.find_by_title('VR Cinema'),
+      App.find_by_title('Half Life 2'),
+      App.find_by_title('Ocean Rift')
+    ]
     @heroes = [App.find_by_title('Faceted Flight')]
     @top_rated = App.all.order('rating DESC').limit(5)
     @newest = App.all.order('created_at DESC').limit(5)
