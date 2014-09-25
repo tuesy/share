1b64a059e1a879009d3932abf9b20662cc87d6d0
Jimmy Zhang <jimmy@gamefuse.com>
2014-09-24 17:14:09 -0700

    add platforms display

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,4 +2,8 @@ class App < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_and_belongs_to_many :genres
+
+  def platforms_array
+    self.platforms.split(',').map(&:strip)
+  end
 end
