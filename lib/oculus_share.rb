47fb9aacceb84a129c950d206b07252819e8ffda
Jimmy Zhang <jimmy@gamefuse.com>
2014-09-26 18:03:17 -0700

    remove legacy files

--- lib/oculus_share.rb
+++
@@ -1,4 +0,0 @@
-# a simple Ruby wrapper for the Share API (see API.md)
-require 'active_resource'
-require_relative 'oculus_share/base'
-require_relative 'oculus_share/app'
--- lib/oculus_share/base.rb
+++
@@ -1,6 +0,0 @@
-module OculusShare
-  class Base < ActiveResource::Base
-    # self.site = 'http://share.mankindforward.com/api'
-    self.site = 'http://localhost:3000/api'
-  end
-end
